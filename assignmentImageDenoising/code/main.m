c22f4ccad7070bb93b368b8f32b56ae0d9804848
Sheshansh Agrawal <sheshansh@cse.iitb.ac.in>
2018-02-17 14:32:26 +0530

    some changed

--- assignmentImageDenoising/code/main.m
+++ assignmentImageDenoising/code/main.m
@@ -1,11 +1,11 @@
 %% Inputting data and setting parameters
 load('../data/assignmentImageDenoisingPhantom.mat')
 shiftedmatrices = [circshift(imageNoisy,1,2),circshift(imageNoisy,-1,2),circshift(imageNoisy,1,1),circshift(imageNoisy,-1,1)];
-x = imageNoisy;
-tau = 0.01;
 
 % %% Quadratic loss
-% alpha = 0.61;
+% x = imageNoisy;
+% tau = 0.01;
+% alpha = 0.38;
 % gamma = 0; % dummy
 % 
 % % Optimisation
@@ -22,45 +22,25 @@ tau = 0.01;
 %     end
 % end
 % 
-% subplot(1,2,1), imshow(real(x));
-% subplot(1,2,2), imshow(real(imageNoisy));
-% temp = (abs(x-imageNoiseless));
-% sum(temp(:))
-% 
-% %% Huber loss
-% alpha = 0.93;
-% gamma = 0.008;
-% 
-% % Optimisation
-% objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@huber,gamma);
-% while tau > 1e-8
-%     delta = delta_function(imageNoisy,x,shiftedmatrices,alpha,@huber_gradient,gamma);
-%     x = x - tau*delta;
-%     objective_function_value_old = objective_function_value;
-%     objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@huber,gamma);
-%     if objective_function_value > objective_function_value_old
-%         tau = 0.5*tau;
-%     else
-%         tau = 1.1*tau;
-%     end
-% end
-% 
-% subplot(1,2,1), imshow(real(x));
-% subplot(1,2,2), imshow(real(imageNoisy));
-% temp = (abs(x-imageNoiseless));
-% sum(temp(:))
+% % subplot(1,2,1), imshow(real(x));
+% % subplot(1,2,2), imshow(real(imageNoisy));
+% absdiff = (abs(imageNoiseless)-abs(x)).^2;
+% noiselesssq = (abs(imageNoiseless)).^2;
+% RRMSE = sum(absdiff(:))/sum(noiselesssq(:))
 
-%% Adaptive loss
-alpha = 0.93;
-gamma = 0.008;
+%% Huber loss
+x = imageNoisy;
+tau = 0.01;
+alpha = 0.9981;
+gamma = 0.010;
 
 % Optimisation
-objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@adaptive,gamma);
+objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@huber,gamma);
 while tau > 1e-8
-    delta = delta_function(imageNoisy,x,shiftedmatrices,alpha,@adaptive_gradient,gamma);
+    delta = delta_function(imageNoisy,x,shiftedmatrices,alpha,@huber_gradient,gamma);
     x = x - tau*delta;
     objective_function_value_old = objective_function_value;
-    objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@adaptive,gamma);
+    objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@huber,gamma);
     if objective_function_value > objective_function_value_old
         tau = 0.5*tau;
     else
@@ -70,5 +50,32 @@ end
 
 subplot(1,2,1), imshow(real(x));
 subplot(1,2,2), imshow(real(imageNoisy));
-temp = (abs(x-imageNoiseless));
-sum(temp(:))
+absdiff = (abs(imageNoiseless)-abs(x)).^2;
+noiselesssq = (abs(imageNoiseless)).^2;
+RRMSE = sum(absdiff(:))/sum(noiselesssq(:))
+
+% %% Adaptive loss
+% x = imageNoisy;
+% tau = 0.01;
+% alpha = 0.9989;
+% gamma = 0.008;
+% 
+% % Optimisation
+% objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@adaptive,gamma);
+% while tau > 1e-8
+%     delta = delta_function(imageNoisy,x,shiftedmatrices,alpha,@adaptive_gradient,gamma);
+%     x = x - tau*delta;
+%     objective_function_value_old = objective_function_value;
+%     objective_function_value = objective_function(imageNoisy,x,shiftedmatrices,alpha,@adaptive,gamma);
+%     if objective_function_value > objective_function_value_old
+%         tau = 0.5*tau;
+%     else
+%         tau = 1.1*tau;
+%     end
+% end
+% 
+% subplot(1,2,1), imshow(real(x));
+% subplot(1,2,2), imshow(real(imageNoisy));
+% absdiff = (abs(imageNoiseless)-abs(x)).^2;
+% noiselesssq = (abs(imageNoiseless)).^2;
+% RRMSE = sum(absdiff(:))%/sum(noiselesssq(:))
