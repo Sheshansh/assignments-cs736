f6cfb7fdcd20a07a0d56fb60a6ebbf08d9ca3d4a
Sheshansh Agrawal <sheshansh@cse.iitb.ac.in>
2018-03-06 01:28:14 +0530

    q3 done

--- assignmentMathImaging/code/q3.m
+++ assignmentMathImaging/code/q3.m
@@ -1,3 +1,37 @@
-You needn't worry about the angles exceeding 180 degrees. All that matters is that only a _subset_ of the range of all possible angles is covered.
+%% CT Chest Image
+load('../data/CT_Chest.mat');
+imagesc(imageAC);colormap(gray);title('Original Chest Image');
+rrmse = zeros(181,1);
+wb = waitbar(0,'Please wait...');
+for theta = 0:1:180
+    reconstructed_image = iradon(radon(imageAC,theta:1:theta+150),theta:1:theta+150);
+    rrmse(theta+1) = RRMSE(imageAC,reconstructed_image(2:513,2:513));
+    waitbar(theta/180);
+end
+close(wb);
+figure;
+plot(rrmse);
+[~,theta] = min(rrmse);
+theta = theta-1;
+fprintf('The optimal angle is theta = %d degrees',theta);
+reconstructed_image = iradon(radon(imageAC,theta:1:theta+150),theta:1:theta+150);
+imagesc(reconstructed_image);colormap(gray);title('Reconstructed Chest Image');
 
-Best
+%% Phantom Image
+load('../data/myPhantom.mat');
+imagesc(imageAC);colormap(gray);title('Original Phantom Image');
+rrmse = zeros(181,1);
+wb = waitbar(0,'Please wait...');
+for theta = 0:1:180
+    reconstructed_image = iradon(radon(imageAC,theta:1:theta+150),theta:1:theta+150);
+    rrmse(theta+1) = RRMSE(imageAC,reconstructed_image(2:257,2:257));
+    waitbar(theta/180);
+end
+close(wb);
+figure;
+plot(rrmse);
+[~,theta] = min(rrmse);
+theta = theta-1;
+fprintf('The optimal angle is theta = %d degrees',theta);
+reconstructed_image = iradon(radon(imageAC,theta:1:theta+150),theta:1:theta+150);
+imagesc(reconstructed_image);colormap(gray);title('Reconstructed Phantom Image');
